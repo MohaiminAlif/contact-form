dc423b30f1554a8bd9d8d8648f357a20ce0389d0
Mohaimin Alif <69976918+MohaiminAlif@users.noreply.github.com>
2021-04-30 23:55:40 +0600

    Update dataViewPage.php

--- dataViewPage.php
+++ dataViewPage.php
@@ -40,6 +40,8 @@ $link = mysqli_connect($host, $user, $password, $db);
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-
+        <div class="container mt-4 text-right";>
+            <a href="adminPage.php" class="btn btn-primary">Go Back Home Page</a>
+        </div>
 </body>
 </html>
